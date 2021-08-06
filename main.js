ac77965816f5ae374ae437b6bcf79b8b6c9fb60e
Tomas Webster <tomasiwebster@gmail.com>
2021-08-06 16:16:58 +1200

    Added better functionality, need to fix leading negation symbol

--- main.js
+++ main.js
@@ -1,51 +1,18 @@
-function add(num1, num2){
-    return num1 + num2;
-}
 
-function subtract(num1, num2){
-    return num1 - num2;
-}
-
-function multiply(num1, num2){
-    return num1 * num2;
-}
-
-function divide(num1, num2){
-    return num1 / num2;
-}
-
-function operator(op,num1, num2){
+function applyOperation(op,num1, num2){
     if (op == "+"){
-        result = add(num1, num2);
+        return num1 + num2;
     }
     if (op == "-"){
-        result = subtract(num1, num2);
+        return num1 - num2;
     }
     if (op == "x"){
-        result = multiply(num1, num2);
+        return num1 * num2;
     }
     if (op == "/"){
-        result = divide(num1, num2);
+        return num1 / num2;
     }
-    return result;
 }
-
-function solve(){
-
-    //solves equation 
-    do{
-        let num1 = numbers[0];
-        let num2 = numbers[1]; 
-        let op = operators[0];
-        let outNum = operator(op, num1, num2);
-        numbers.shift();
-        numbers[0] = String(outNum);
-        operators.shift();
-    }while(operators.length > 0);
-    currentNum == "";
-    return numbers;
-}
-
 
 function isInt(str){
     //Checks if a string can be an int i.e if '7' can be 7 not 'a' can be int.
@@ -57,9 +24,59 @@ function isInt(str){
     }
 }
 
+function precendence(op){
+    if (op == "+" || op == "-"){
+        return 1;
+    }
+    if (op == "x" || op == "/"){
+        return 2;
+    }
+    return 0;
+}
 
-let numbers = [];
-let operators = [];
+function solve(eq){
+    //Create stack for integer values and operators
+    let values = [];
+    let ops = [];
+    i = 0;
+
+    while (i < eq.length){
+        if (eq[i] == " "){
+            i+= 1;
+            continue;
+        }
+        else if (isInt(eq[i])){
+            val = 0;
+
+            while (i < eq.length && isInt(eq[i])){
+                val = (val * 10) + parseInt(eq[i]);
+                i+=1;
+            }
+            values.push(val);
+            i-=1;
+        }else{
+            while(ops.length != 0 && precendence(ops[ops.length-1]) >= precendence(eq[i])){
+                let val2 = values.pop();
+                let val1 = values.pop();
+                let op = ops.pop();
+
+                values.push(applyOperation(op, val1, val2));
+            }
+            ops.push(eq[i]);
+        }
+        i+=1;
+    }
+    while (ops.length != 0){
+        let val2 = values.pop();
+        let val1 = values.pop();
+        let op = ops.pop();
+        values.push(applyOperation(op,val1,val2));
+    }
+    return values[values.length-1];
+}
+
+
+let equation = "";
 const outScreen = document.querySelector("#outScreen");
 const buttonContainer = Array.from(document.querySelector("#buttonContainer").children);
 let currentNum = "";
@@ -71,22 +88,27 @@ buttonContainer.forEach(button => {
         // Creates button logic for numerical buttons
         button.onclick = function(){
             outScreen.innerHTML += id;
+            equation += id;
+            
         }
     }
     if (id == "delete"){
         //Deletes last character 
         button.onclick = function(){
             outScreen.innerHTML = (outScreen.innerHTML).slice(0,-1);
+            equation = equation.slice(0,-1);
         }
     }
     if (id =="decimal"){
         button.onclick = function(){
             outScreen.innerHTML += ".";
+            equation += ".";
         }
     }
     if (id == "reset"){
         button.onclick = function(){
             outScreen.innerHTML = "";
+            equation = "";
         }
     }
     if (id == "division"||
@@ -95,6 +117,23 @@ buttonContainer.forEach(button => {
         id == "subtraction"){
             button.onclick = function(){
                 outScreen.innerHTML += button.innerHTML;
+                equation += button.innerHTML;
             }
         }
+    if (id == "equals"){
+        button.onclick = function(){
+            equation = solve(equation);
+            outScreen.innerHTML = equation;
+        }
+    }
+    if (id == "plusMinus"){
+        button.onclick = function(){
+            if (isInt(equation)){
+                let num = parseInt(equation);
+                if (num < 0){
+                    equation = equation.slice(1);
+                }
+            }
+        }
+    }
 });
